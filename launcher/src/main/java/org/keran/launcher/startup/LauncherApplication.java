09c102ebace6ffa8265083579025c3cd4c9b0fb9
Jaroslaw Jarosz <jarekkeran@gmail.com>
2023-08-17 14:23:08 +0200

    feature: Launcher application prepared

--- launcher/src/main/java/org/keran/launcher/startup/LauncherApplication.java
+++ launcher/src/main/java/org/keran/launcher/startup/LauncherApplication.java
@@ -1,4 +1,22 @@
 package org.keran.launcher.startup;
 
-public class LauncherApplication {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(scanBasePackages={"org.keran"})
+@ComponentScan(basePackages = {"org.keran"})
+public class LauncherApplication implements CommandLineRunner {
+
+
+    public static void main(String[] args){
+        SpringApplication.run(LauncherApplication.class, args);
+        System.out.println("main method");
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Hola chicos!");
+    }
 }

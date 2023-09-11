cbfbfd29e5d8276011fefbfbb30f017913005649
Jaroslaw Jarosz <jarekkeran@gmail.com>
2023-09-11 14:22:29 +0200

    feature: added image, added get method

--- domain/src/main/java/org/keran/domain/ports/api/loyaltyProgram/LoyaltyProgramFindServicePort.java
+++ domain/src/main/java/org/keran/domain/ports/api/loyaltyProgram/LoyaltyProgramFindServicePort.java
@@ -2,11 +2,13 @@ package org.keran.domain.ports.api.loyaltyProgram;
 
 import org.keran.domain.data.loyaltyProgram.LoyaltyProgramDto;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface LoyaltyProgramFindServicePort {
     Optional<LoyaltyProgramDto> findLoyaltyProgramById(UUID id);
+    List<LoyaltyProgramDto> getLoyaltyPrograms(int pageSize, int currentPage);
     Optional<LoyaltyProgramDto> findLoyaltyProgramByName(String name);
     boolean existsById(UUID id);
     boolean existsByName(String name);

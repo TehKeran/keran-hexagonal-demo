2ee61dddb5d21d9dcc57ee82a075b8519327806e
Jaroslaw Jarosz <jarekkeran@gmail.com>
2023-09-11 14:23:01 +0200

    feature: added image, added get method

--- infrastructure/src/main/java/org/keran/infrastructure/repository/AbstractRepository.java
+++
@@ -1,7 +0,0 @@
-package org.keran.infrastructure.repository;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@CrossOrigin("http://localhost:4200")
-public interface AbstractRepository {
-}
